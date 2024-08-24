a9f17ada6f6b1c36fa360ee58134979e87be8ef2
Luskas Walker <lucascompanyk@gmail.com>
2024-08-24 18:20:30 -0300

    Aplicando logica de rotas, integração com API e desings

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj:any = {
+    "EmailId" : "",
+    "Password": ""
+  };
+
+  http = inject(HttpClient);
+
+  constructor(private router:Router){
+
+  }
+
+  onLogin() {
+    this.http.post("https://freeapi.miniprojectideas.com/api/User/Login", this.loginObj).subscribe((res:any) => {
+      if (res.result) {
+        alert("Login Succes");
+        this.router.navigateByUrl("dashboard")
+      } else {
+        alert("Invalid Email or Password");
+      }
+    })
+  }
 
 }
